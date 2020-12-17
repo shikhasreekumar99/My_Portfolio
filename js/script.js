ff8a770726b2285d5d07df4efbc6cbc36a6a6761
Shikha Sreekumar <shikha@qworks.co>
2020-12-17 11:12:41 +0530

    SS2012171112 My Initial Commit

---
+++ js/script.js
@@ -0,0 +1,183 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Designer", "Developer", "Blogger", "Deployer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Designer", "Developer", "Blogger", "Deployer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+document.onkeydown = function(e) {
+      if(event.keyCode == 123) {
+         return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+         return false;
+      }
+    }
+
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+
+
+
+
+
+//MAIL SERVICE
+// $(document).ready(function(){  
+//  $("#contact-form").validate({
+          
+//     submitHandler: function(form) {          
+//            $.ajax({
+//                  type: 'POST',
+//                  url: "mail/contact_mail.php",
+//                  data: $('#contact-form').serialize(),   // I WANT TO ADD EXTRA DATA + SERIALIZE DATA
+//                  success: function(msg){
+                     
+//                         $('#c_thankyou').show();
+//                      $('form[name="contact-form"]')[0].reset();
+                     
+//                      }
+//            });                                     
+//           return false; 
+
+//         }     
+//     });
+// });
+
+// Variable to hold request
+var request;
+
+// Bind to the submit event of our form
+$("#contact-form").submit(function(event){
+   
+    // Prevent default posting of form - put here to work in case of errors
+    event.preventDefault();
+
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        type: 'POST',
+        url: "mail/contact_mail.php",
+        data: $('#contact-form').serialize(),   // I WANT TO ADD EXTRA DATA + SERIALIZE DATA
+                 success: function(msg){
+                     $('#c_thankyou').show();
+                     $('form[name="contact-form"]')[0].reset();
+                     
+                     } 
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+});
---
+++ mail/contact_mail.php
@@ -0,0 +1,93 @@
+<?php
+
+
+		session_start();
+
+
+		ob_start();
+
+
+			//create PHP Mailer object
+
+
+			require("class.phpmailer.php");
+
+
+			$mailer = (object) new PHPmailer();
+
+
+			//setup PHPMailer
+
+
+			$your_Email = "shikhasreekumar99@gmail.com";
+
+			$mailer->From ='shikhasreekumar99@gmail.com';
+              
+
+			$mailer->FromName ='Shikha Sreekumar';  
+
+
+			$mailer->AddAddress($your_Email);
+
+
+			$mailer->Subject = "Contact Us";
+			
+			
+
+
+			$mailer->Body = "<table cellpadding=5 cellspacing=5>
+			
+			<tr><td align=right><b>Name:&nbsp;</b></td><td>".$_POST['name']."</td></tr>
+			
+			<tr><td align=right><b>Email:&nbsp;</b></td><td>".$_POST['email']."</td></tr>
+			
+			<td align=right valign=top><b>Message:&nbsp;</b></td><td style='text-align:justify;'>".$_POST['message']."</td></tr>
+			
+			</table>";
+
+
+			//send mail as HTML
+
+
+			$mailer->IsHTML(true);
+
+
+			//set charset
+
+
+			$mailer->CharSet = "utf-8";
+
+
+			//set mailing method... mail, smtp or sendmail
+
+
+			$mailer->Mailer = "mail";
+
+
+			if ($mailer->Send())
+
+
+        	{
+				
+				echo "<span style='color:red;'><b> MAIL SENT </b></span>";
+				
+				}
+
+
+        	else
+
+
+        	{
+
+
+				echo "<span style='color:red;'><b>FAIL TO SENT MAIL</b></span>";
+
+
+        	}
+
+
+
+		ob_end_flush();
+
+
+?>
